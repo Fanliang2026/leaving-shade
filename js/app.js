b11703732369926c61f045a53dc73ac5203d6e3c
Fanliang2026 <lyon4691@gmail.com>
2026-07-18 23:17:07 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
-// Project Shade v0.1
+/* ==========================================
+   Project Shade
+   app.js
+   Version 1.0
+========================================== */
+
+
+/* ==========================================
+   QUOTATION
+========================================== */
+
+const quote =
+
+"The tree may fall. But the shade it provided will continue to shelter those who come after.";
+
+
+
+/* ==========================================
+   PAGE ELEMENTS
+========================================== */
+
+const quoteText =
+document.getElementById("quoteText");
+
+const startButton =
+document.getElementById("startButton");
+
+const title =
+document.querySelector(".title");
+
+const subtitle =
+document.querySelector(".subtitle");
+
+const author =
+document.querySelector(".author");
+
+const seed =
+document.getElementById("seed");
